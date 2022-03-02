9bf4b37d1c7affda54f37e636a0a69ed22b61f01
Idalina Sachango <idalinassachango@MacBook-Pro-4.local>
2022-03-02 02:25:27 -0600

    Created a chicago map with dummy data; to integrate to Django

--- maps/choropleth.py
+++ maps/choropleth.py
@@ -3,22 +3,23 @@ import plotly.express as px
 import pandas as pd
 from urllib.request import urlopen
 
-df = pd.DataFrame(pd.read_csv("data/School_Locations_SY1718.csv", dtype={"School_ID": str}))
-df = df[["School_ID", "the_geom","Lat", "Long", "WARD_15"]]
+df = pd.read_csv("data/School_Locations_SY1718.csv", dtype={"COMMAREA": str})
 df = df.fillna(999)
-df = df.astype({"School_ID": int, "Lat": int, "Long": int, "WARD_15": str})
 
-with open("data/cps-1718.geojson") as f:
+with open("data/chicago_boundaries.geojson") as f:
     jsn = json.load(f)
-    print(jsn["features"][0]["properties"]["school_id"])
 
 for i in range(len(jsn["features"])):
-    jsn["features"][i]["id"] = jsn["features"][i]["properties"]["school_id"]
+    jsn["features"][i]["id"] = jsn["features"][i]["properties"]["community"]
 
-fig = px.choropleth(df, geojson=jsn,
-                           locations='School_ID',
-                           color="WARD_15"
+print(px.data.election().keys())
+print(px.data.election_geojson())
+
+
+fig = px.choropleth(df, geojson=jsn, locations='COMMAREA', color='WARD_ADJ',
+                           color_continuous_scale="Viridis",
+                           scope="usa"
                           )
+fig.update_geos(fitbounds="locations", visible=True)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
-
