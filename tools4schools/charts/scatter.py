4ed1a5a39ba8b238fc0fb1649c68c5b678baeea1
Idalina <idalina@uchicago.edu>
2022-03-14 11:42:41 -0500

    Got coloring to work and removed legend

--- tools4schools/charts/scatter.py
+++ tools4schools/charts/scatter.py
@@ -8,7 +8,8 @@ from pathlib import Path
 
 def make_fig():
     home_path = Path(__file__).parent.parent
-    data_path = home_path.joinpath("data")
+    data_path = home_path.joinpath("Project/Output")
+    data_test = home_path.joinpath("data")
     data_path_geo = home_path.joinpath("data/geojson")
 
 
@@ -19,58 +20,42 @@ def make_fig():
             df["features"][i]["id"] = df["features"][i]["properties"][feature]
         return df
 
-    cps_locations = open_path("cps-geojson.geojson", "school_id")
+    #cps_locations = open_path("cps-geojson.geojson", "school_id")
     census_tract = open_path("census_tract.geojson", "geoid10")
 
-
-    cps_bounds = json_normalize(cps_locations["features"])
-    coords = 'geometry.coordinates'
-    df = (cps_bounds[coords].apply(lambda r: [(r[0],r[1])])
-            .apply(pd.Series).stack()
-            .reset_index(level=1).rename(columns={0:coords,"level_1":"point"})
-            .join(cps_bounds.drop(coords,1), how='left')).reset_index(level=0)
-    df[['lat','long']] = df[coords].apply(pd.Series)
-
-    df = pd.DataFrame(df)
-    df = df.rename(columns={"properties.school_id": "schoolid"})
-    df = df.astype({"id": str})
-
-
-    df2 = pd.read_csv(data_path.joinpath("college.csv"), dtype={"schoolid": str})
-    df2 = df2.fillna(999)
+    df2 = pd.read_csv(data_path.joinpath("opportunity_index_by_school_scaled.csv"), dtype={"School ID": str})
     df2 = pd.DataFrame(df2)
+    df2 = df2.assign(opportunity_ranked = pd.qcut(df2.opportunity_index, 5, labels = [1, 2, 3, 4, 5]))
 
     #merge cps college data with csv with latitude longitude for schools
-    full = pd.merge(df2, df, on='schoolid',  how='left')
+    #full = pd.merge(df2, df, on='schoolid',  how='left')
 
-    df3 = pd.read_csv(data_path.joinpath("chi_air.csv"), dtype={"ctfips": str})
+    df3 = pd.read_csv(data_test.joinpath("chi_air.csv"), dtype={"ctfips": str})
     df3 = df3.fillna(999)
     df3 = pd.DataFrame(df3)
     df3["blank_bounds"] = 0
 
-    colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
+    #colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
     fig = go.Figure()
 
     #plot bubbles with cps college data
-    for _, row in full.iterrows():
+    for _, row in df2.iterrows():
         fig.add_trace(go.Scattergeo(
-        lon = [row['properties.long']],
-        lat = [row['properties.lat']],
-        marker = dict(
-            #size=row['pctenroll17']/100,
-            # line_color='rgb(40,40,40)',
-            # line_width=0.5,
-            # sizemode = 'area'
-            color=[row['pctenroll17']/100],
-            colorscale=["blue", "red"] 
-        )))
+        lon = [row['longitude']],
+        lat = [row['latitude']],
+        showlegend=False,
+        marker_color=[row['opportunity_ranked']],
+        marker_cmin=1,
+        marker_cmax=5,
+        marker_colorscale='ylorrd'))
 
-    # draw census tract boundaries
+    # draw census tract boundariess
     fig.add_trace(go.Choropleth(
         geojson=census_tract,
         featureidkey="properties.geoid10",
         locations=df3["ctfips"],
-        z = df3["blank_bounds"]
+        z = df3["blank_bounds"],
+        showscale=False
     ))
 
     fig.update_geos(fitbounds="locations", visible=False)
@@ -79,7 +64,9 @@ def make_fig():
 
     fig.update_layout(
         title_text = 'College Enrollment by School',
-        geo_scope='usa', # limite map scope to USA
+        geo_scope='usa', # limite map scope to USA,
+        showlegend=False
     )
+    fig.update_coloraxes(showscale=False)
 
     return fig
