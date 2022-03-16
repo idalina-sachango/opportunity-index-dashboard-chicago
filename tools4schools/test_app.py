f15d3eddf2fc99702861164d03b72c629e749c52
Akila Forde <aforde@linux1.cs.uchicago.edu>
2022-03-16 13:51:15 -0500

    Pylint

--- tools4schools/test_app.py
+++ tools4schools/test_app.py
@@ -1,11 +1,73 @@
+'''
+Dash App for Tools4Schools
 
-import dash
+Output layout including 3 visulations and a table of
+the resulting data using callbacks
+
+'''
+from pathlib import Path
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
-from tools4schools.charts import run_all, enroll_scatter
 import flask
 import pandas as pd
-from pathlib import Path
+import dash
+from tools4schools.charts import run_all, enroll_scatter
+
+
+def generate_table(dataframe):
+    '''
+    Creates a Dash Datatable to output on the dash App with the school's
+    opportunity index and a variety of indicators.
+
+    Inputs:
+        dataframe (pandas df): dataframe
+
+    Returns: dashtable
+    '''
+    table = dash_table.DataTable(data=dataframe.to_dict('records'),
+            columns=[
+            {'name': 'School Name', 'id': 'school_name', 'type': 'text'},
+            {'name': 'Census Tract', 'id': 'census_tract', 'type': 'numeric'},
+            {'name': 'Opportunity Index', 'id': 'opportunity_index',
+                    'type': 'numeric'},
+            {'name': 'Student Enrollment', 'id': 'enrollment_crdc',
+                'type': 'numeric'},
+            {'name': '% College enrollment', 'id': 'college_enroll_pct',
+                'type': 'numeric'},
+            {'name': 'Free or Reduced Price Lunch',
+                'id': 'free_or_reduced_price_lunch', 'type': 'numeric'},
+            {'name': '# Certified Teachers',
+                'id': 'teachers_certified_fte_crdc', 'type': 'numeric'},
+            {'name': '# Counselors', 'id': 'counselors_fte_crdc',
+                'type': 'numeric'},
+            {'name': '# Law Enforcement Officers',
+                'id': 'law_enforcement_fte_crdc', 'type': 'numeric'},
+            {'name': 'Average Salaries', 'id': 'salaries_crdc',
+                'type': 'numeric'},
+            {'name': '# Students Enrolled in AP courses', 'id': 'enrl_AP_crdc',
+                    'type': 'numeric'},
+            {'name': 'FY 2017 Ending Budget', 'id': 'FY 2017 Ending Budget',
+                'type': 'numeric'},
+            {'name': '% Households with Internet subscription',
+                'id': 'internet_rate', 'type': 'numeric'},
+            {'name': 'Employment Rate for Individuals 25-64 years',
+                'id': 'emp_rate_25_64', 'type': 'numeric'},
+            {'name': '% Above Poverty Rate', 'id': 'above_pov_rate',
+                'type': 'numeric'},
+            {'name': '% Households on Food Stamps', 'id': 'food_stamps',
+                'type': 'numeric'},
+            {'name': 'Median Household Income', 'id': 'median_earnings',
+                'type': 'numeric'}
+            ],
+            style_table={'height': 400,'overflowX': 'auto',},
+            style_data={
+                        'width': '150px', 'minWidth': '150px',
+                        'maxWidth': '150px',
+                        'overflow': 'hidden',
+                        'textOverflow': 'ellipsis',
+                    },
+            page_size=10)
+    return table
 
 external_stylesheets = [
     {
@@ -17,7 +79,8 @@ external_stylesheets = [
 
 server = flask.Flask(__name__)
 ### Dash App
-app = dash.Dash(__name__, server = server, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, server = server,
+            external_stylesheets=external_stylesheets)
 
 
 ### Figures
@@ -25,15 +88,16 @@ run_all_fig = run_all.run_all()
 enroll_scatter_fig = enroll_scatter.make_fig()
 
 
-### Table
-
 ### Table Data
 home_path = Path(__file__).parent
 data_path = home_path.joinpath("data/results")
 df1 = pd.read_csv(data_path.joinpath('opportunity_index_by_school_scaled.csv'))
 df2 = pd.read_csv(data_path.joinpath('indicators_by_school_unscaled.csv'))
+df2['college_enroll_pct'] = df2['college_enroll_pct']*100
 
-merged_df = pd.merge(df1[['opportunity_index', 'ncessch', 'enrollment_crdc']], df2, how='left')
+merged_df = pd.merge(df1[['opportunity_index', 'ncessch',
+                            'enrollment_crdc']],
+                    df2, how='left')
 
 table_df = merged_df[['school_name', 'census_tract', 'opportunity_index',
        'college_enroll_pct', 'free_or_reduced_price_lunch',
@@ -43,47 +107,6 @@ table_df = merged_df[['school_name', 'census_tract', 'opportunity_index',
        'FY 2017 Ending Budget', 'food_stamps', 'median_earnings',
        'ds_pm_pred']]
 
-
-def generate_table(dataframe):
-    '''
-    Creates a Dash Datatable to output on the dash App with the school's opportunity index
-    and a variety of indicators.
-
-    Inputs:
-        dataframe (pandas df): dataframe 
-    
-    Returns: dashtable
-    '''
-    table = dash_table.DataTable(data=dataframe.to_dict('records'),
-            columns=[
-            {'name': 'School Name', 'id': 'school_name', 'type': 'text'},
-            {'name': 'Census Tract', 'id': 'census_tract', 'type': 'numeric'},
-            {'name': 'Opportunity Index', 'id': 'opportunity_index', 'type': 'numeric'},
-            {'name': 'Student Enrollment', 'id': 'enrollment_crdc', 'type': 'numeric'},
-            {'name': '% College enrollment', 'id': 'college_enrollment_pct', 'type': 'numeric'},
-            {'name': 'Free or Reduced Price Lunch', 'id': 'free_or_reduced_price_lunch', 'type': 'numeric'},
-            {'name': '# Certified Teachers', 'id': 'teachers_certified_fte_crdc', 'type': 'numeric'},
-            {'name': '# Counselors', 'id': 'counselors_fte_crdc', 'type': 'numeric'},
-            {'name': '# Law Enforcement Officers', 'id': 'law_enforcement_fte_crdc', 'type': 'numeric'},
-            {'name': 'Average Salaries', 'id': 'salaries_crdc', 'type': 'numeric'},
-            {'name': '# Students Enrolled in AP courses', 'id': 'enrl_AP_crdc', 'type': 'numeric'},
-            {'name': 'FY 2017 Ending Budget', 'id': 'FY 2017 Ending Budget', 'type': 'numeric'},
-            {'name': '% Households with Internet subscription', 'id': 'internet_rate', 'type': 'numeric'},
-            {'name': 'Employment Rate for Individuals 25-64 years', 'id': 'emp_rate_25_64', 'type': 'numeric'},
-            {'name': '% Above Poverty Rate', 'id': 'above_pov_rate', 'type': 'numeric'},
-            {'name': '% Households on Food Stamps', 'id': 'food_stamps', 'type': 'numeric'},
-            {'name': 'Median Household Income', 'id': 'median_earnings', 'type': 'numeric'}
-            ],
-            style_table={'height': 400,'overflowX': 'auto',},
-            style_data={
-                        'width': '150px', 'minWidth': '150px', 'maxWidth': '150px',
-                        'overflow': 'hidden',
-                        'textOverflow': 'ellipsis',
-                        # 'backgroundColor': 'rgb(50, 50, 50)',
-                        # 'color':'white',
-                    },
-            page_size=10)
-    return table
 
 
 
@@ -137,8 +160,8 @@ app.layout = html.Div(
 
 
 ### Dropdown callback for DataTable
-@app.callback(dash.dependencies.Output('table-container', 'children'),
-    [dash.dependencies.Input('dropdown', 'value')])
+@app.callback(Output('table-container', 'children'),
+    [Input('dropdown', 'value')])
 
 def display_table(dropdown_value):
     if dropdown_value is None:
