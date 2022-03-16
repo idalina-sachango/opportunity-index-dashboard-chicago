75033a1cf47f9d0daf2032b4d995e1ef554c6515
Idalina Sachango <idalinassachango@MacBook-Pro-4.local>
2022-03-16 13:19:15 -0500

    Added and updated chart files

---
+++ tools4schools/charts/run_all.py
@@ -0,0 +1,80 @@
+from asyncio import run
+import plotly.graph_objects as go
+import pandas as pd
+import json
+import geopandas as gpd
+from pandas.io.json import json_normalize
+from pathlib import Path
+from tools4schools.charts import scatter, college, poverty_rate, budget
+
+
+def run_all():
+
+    scatter_fig = scatter.make_fig()
+    college_fig = college.make_fig()
+    poverty_fig = poverty_rate.make_fig()
+    budget_fig = budget.make_fig()
+
+    fig = go.Figure()
+
+    fig.update_geos(fitbounds="locations", visible=True)
+    fig.add_traces(scatter_fig.data)
+    fig.add_traces(college_fig.data)
+    fig.add_traces(poverty_fig.data)
+    fig.add_traces(budget_fig.data)
+
+
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
+    hovermode='x unified')
+
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                buttons=list([
+                    dict(label="Opportunity Index by School",
+                         method="update",
+                         args=[{"visible": [False,True,True,
+                                            True,True,True,
+                                            False,False,False,
+                                            False,False,False,
+                                            False,False]},
+                               {"title": "Opportunity index mapping"}]),
+                    dict(label="College Enrollment Percentage by School",
+                         method="update",
+                         args=[{"visible": [False,False,False,
+                                            False,False,False,
+                                            True,True,False,
+                                            False,False,False,
+                                            False,False]},
+                               {"title": "College enrollment index mapping"}]),
+                    dict(label="Poverty Rate x College Enrollment",
+                         method="update",
+                         args=[{"visible": [False,False,False,
+                                            False,False,False,
+                                            False,False,True,
+                                            True,False,False,
+                                            False,False]},
+                               {"title": "College enrollment index mapping to poverty rate"}]),
+                    dict(label="Budget by school",
+                         method="update",
+                         args=[{"visible": [False,False,False,
+                                            False,False,False,
+                                            False,False,False,
+                                            False,True,True,
+                                            True,True]},
+                               {"title": "Budget by school"}])
+                ]),
+                type = "buttons",
+                direction="left",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=0.2,
+                xanchor="left",
+                yanchor="top"
+
+            )
+
+        ]
+
+    )
+    return fig
--- tools4schools/charts/scatter.py
+++ tools4schools/charts/scatter.py
@@ -3,9 +3,9 @@ import plotly.graph_objects as go
 import pandas as pd
 import json
 import geopandas as gpd
+import numpy as np
 from pandas.io.json import json_normalize
 from pathlib import Path
-
 
 
 def make_fig():
@@ -29,26 +29,55 @@ def make_fig():
     census_df["blank_bounds"] = 0
     census_df = census_df[["geoid10", "blank_bounds"]]
 
+    #cps_locations = open_path("cps-geojson.geojson", "school_id")
+    census_tract = open_path("census_tract.geojson", "geoid10")
+
     df2 = pd.read_csv(data_path.joinpath("opportunity_index_by_school_scaled.csv"), dtype={"School ID": str})
     df2 = pd.DataFrame(df2)
-    df2 = df2.assign(opportunity_ranked = pd.qcut(df2.opportunity_index, 5, labels = [1, 2, 3, 4, 5]))
+    # df2 = df2.assign(opportunity_ranked = pd.qcut(df2.opportunity_index, 5, labels = [1, 2, 3, 4, 5]))
+
+    df2['opportunity_ranked'] = np.select(
+        [
+            df2['opportunity_index'].between(0, 20, inclusive=True), 
+            df2['opportunity_index'].between(20, 40, inclusive='right'),
+            df2['opportunity_index'].between(40, 60, inclusive='right'),
+            df2['opportunity_index'].between(60, 80, inclusive='right'),
+            df2['opportunity_index'].between(80, 100, inclusive='right')
+        ], 
+        [
+            1, 
+            2,
+            3,
+            4,
+            5
+        ], 
+        default='Unknown'
+    )
+
+    pd.to_numeric(df2['opportunity_ranked'])
 
     fig = go.Figure()
-    opp = df2["opportunity_index"]
 
-    #plot points with opp index data
-    fig.add_trace(go.Scattergeo(
-        lon = df2['longitude'],
-        lat = df2['latitude'],
-        text = df2["school_name"],
-        marker_color=df2['opportunity_ranked'],
+
+    for _, row in enumerate(df2['opportunity_ranked'].unique()):
+        fig.add_trace(go.Scattergeo(
+        lon = df2['longitude'][df2['opportunity_ranked'] == row],
+        lat = df2['latitude'][df2['opportunity_ranked'] == row],
+        text = df2['school_name'][df2['opportunity_ranked'] == row],
+        marker_color=df2['opportunity_ranked'][df2['opportunity_ranked'] == row].astype(int),
         marker_cmin=1,
         marker_cmax=5,
-        marker_colorscale='rdylgn'))
+        marker_colorscale='rdylgn',
+        marker_size=10,
+        showlegend=True,
+        name=row,
+        customdata=df2["opportunity_index"][df2['opportunity_ranked'] == row],
+        hovertemplate='<b>School Name<extra></extra></b>: %{text}<br>' + 
+                                        '<b>Opportunity Index:</b> %{customdata}'
+        
+        
+        ))
 
-    fig.update_traces(customdata=df2["opportunity_index"])
-    fig.update_traces(hovertemplate='<b>School Name<extra></extra></b>: %{text}<br>' + 
-                                    '<b>Opportunity Index:</b> %{customdata}')
 
     # draw census tract boundariess
     fig.add_trace(go.Choropleth(
